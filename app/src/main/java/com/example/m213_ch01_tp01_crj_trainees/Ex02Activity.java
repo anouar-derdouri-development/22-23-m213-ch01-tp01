b941bac92d17eee629f403dcfae0e8d17a1c2a42
Anouar DERDOURI <anouar.derdouri.development@gmail.com>
2023-02-28 14:30:35 +0100

    Ex02: Layout

---
+++ app/src/main/java/com/example/m213_ch01_tp01_crj_trainees/Ex02Activity.java
@@ -0,0 +1,14 @@
+package com.example.m213_ch01_tp01_crj_trainees;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ex02Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ex02);
+    }
+}
