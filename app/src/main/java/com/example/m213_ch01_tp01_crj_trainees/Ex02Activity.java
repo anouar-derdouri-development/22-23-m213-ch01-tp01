02012ed10662c359773a0414c38f26b66bd533d9
Anouar DERDOURI <anouar.derdouri.development@gmail.com>
2023-03-01 19:05:57 +0100

    Ex02: Java

--- app/src/main/java/com/example/m213_ch01_tp01_crj_trainees/Ex02Activity.java
+++ app/src/main/java/com/example/m213_ch01_tp01_crj_trainees/Ex02Activity.java
@@ -1,14 +1,59 @@
 package com.example.m213_ch01_tp01_crj_trainees;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Ex02Activity extends AppCompatActivity {
+    EditText etEx02Nombre;
+    TextView tvEx02Parite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ex02);
+
+        etEx02Nombre = findViewById(R.id.etEx02Nombre);
+        tvEx02Parite = findViewById(R.id.tvEx02Parite);
+
+//        tvEx02Parite.setTextColor(Color.RED);
+
+        etEx02Nombre.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                try {
+                    int nombre = Integer.parseInt(etEx02Nombre.getText().toString());
+
+                    tvEx02Parite.setTextColor(Color.GREEN);
+
+                    if (nombre % 2 == 0)
+                        tvEx02Parite.setText(String.format("%d est pair", nombre));
+                    else
+                        tvEx02Parite.setText(String.format("%d est impair", nombre));
+                } catch (NumberFormatException e) {
+                    tvEx02Parite.setTextColor(Color.RED);
+                    tvEx02Parite.setText("?");
+                }
+            }
+        });
+
+        etEx02Nombre.setText("");
     }
 }
